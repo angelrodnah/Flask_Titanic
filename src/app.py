f002dc59c0d67576723c3c2e7940adbe8acc10f9
ArnSener <angel.rodrigo@sener.es>
2024-03-20 09:54:08 +0100

    e

--- src/app.py
+++ src/app.py
@@ -1,13 +1,29 @@
-# importing the packages
+# Importar Librerias	
 import json
-# import pandas as pd
+import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify, render_template, flash#, redirect, url_for, session, logging
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-from sklearn.externals import joblib
-from sklearn.preprocessing import LabelEncoder
+from wtforms import Form, StringField, TextAreaField, validators, StringField, SubmitField
+import joblib
+
+# Preprocessing
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import *
+
+# Modelos
+from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+#from catboost import CatBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier, BaggingClassifier
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, f1_score, precision_score, recall_score, \
+roc_curve, roc_auc_score, ConfusionMatrixDisplay, multilabel_confusion_matrix
 
 # value of __name__ should be  '__main__'
 app = Flask(__name__)
