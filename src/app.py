45257c987d17d2f9d2057bc8401de488f9d3343f
ArnSener <angel.rodrigo@sener.es>
2024-03-21 01:10:53 +0100

    2

--- src/app.py
+++ src/app.py
@@ -1,44 +1,44 @@
-# Importing necessary libraries
+# Importando las bibliotecas necesarias
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
 from wtforms import Form, StringField, validators
 import dill
 
-# Preprocessing
+# Preprocesamiento
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-# Value of __name__ should be '__main__'
+# El valor de __name__ debería ser '__main__'
 app = Flask(__name__)
 
-# Loading model and encoder
+# Cargando el modelo y el codificador
 with open('./model/model.pkl', 'rb') as model_file:
     model = dill.load(model_file)
 
 
-# Defining a form class to retrieve input from the user through a form
+# Definiendo una clase de formulario para recuperar la entrada del usuario a través de un formulario
 class PredictorsForm(Form):
-    p_class = StringField(u'P Class (Valid Values: 1, 2, 3)', validators=[validators.input_required()])
-    sex = StringField(u'Sex (0: Female and 1: Male)', validators=[validators.input_required()])
-    age = StringField(u'Age (For example: 24)', validators=[validators.input_required()])
-    sibsp = StringField(u'Siblings and Spouse Count (For example: 3)', validators=[validators.input_required()])
-    parch = StringField(u'Parch (Valid Values: 0, 1, 2, 3, 4, 5, 6)', validators=[validators.input_required()])
-    fare = StringField(u'Fare (For example: 100)', validators=[validators.input_required()])
-    embarked = StringField(u'Embarked (Valid Values: 0, 1, 2)', validators=[validators.input_required()])
+    p_class = StringField(u'Clase de Pase (Valores válidos: 1, 2, 3)', validators=[validators.input_required()])
+    sex = StringField(u'Sexo (0: Femenino y 1: Masculino)', validators=[validators.input_required()])
+    age = StringField(u'Edad (Por ejemplo: 24)', validators=[validators.input_required()])
+    sibsp = StringField(u'Número de hermanos/cónyuge (Por ejemplo: 3)', validators=[validators.input_required()])
+    parch = StringField(u'Parch (Valores válidos: 0, 1, 2, 3, 4, 5, 6)', validators=[validators.input_required()])
+    fare = StringField(u'Tarifa (Por ejemplo: 100)', validators=[validators.input_required()])
+    embarked = StringField(u'Embarcado (Valores válidos: 0, 1, 2)', validators=[validators.input_required()])
 
-# Index page
+# Página de índice
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# About me page
+# Página sobre mí
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Prediction route
+# Ruta de predicción
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     form = PredictorsForm(request.form)
@@ -52,11 +52,11 @@ def predict():
         fare = form.fare.data
         embarked = form.embarked.data
 
-        # Creating input for model predictions
+        # Creando la entrada para las predicciones del modelo
         predict_request = [int(p_class), int(sex), float(age), int(sibsp), int(parch), float(fare), int(embarked)]
         predict_request = np.array(predict_request).reshape(1, -1)
 
-        # Making predictions using the model
+        # Realizando predicciones usando el modelo
         prediction = model.predict(predict_request)[0]
         predict_prob = model.predict_proba(predict_request)[0][1]
 
@@ -64,44 +64,43 @@ def predict():
 
     return render_template('predict.html', form=form)
 
-# Training route
+# Ruta de entrenamiento
 @app.route('/train', methods=['GET'])
 def train():
-    # Reading data
+    # Leyendo los datos
     df = pd.read_csv("./data/titanic.csv")
 
-    # Defining predictors and label columns
+    # Definiendo las columnas predictoras y de etiqueta
     predictors = ['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
     label = 'Survived'
 
-    # Splitting data into training and testing
+    # Dividiendo los datos en entrenamiento y prueba
     df_train, df_test, y_train, y_test = train_test_split(df[predictors], df[label], test_size=0.20, random_state=42)
 
-    # Data cleaning and filling missing values
+    # Limpieza de datos y rellenado de valores faltantes
     df_train.Age = df_train.Age.fillna(df.Age.mean())
     df_train.Embarked = df_train.Embarked.fillna(df_train.Embarked.mode()[0])
 
-    # Encoding categorical variables
+    # Codificación de variables categóricas
     for column in df_train.columns:
         if df_train[column].dtype == np.object:
             le = LabelEncoder()
             df_train[column] = le.fit_transform(df_train[column])
 
-    # Initializing the model
+    # Inicializando el modelo
     model = RandomForestClassifier(n_estimators=25, random_state=42)
 
-    # Fitting the model with training data
+    # Ajustando el modelo con los datos de entrenamiento
     model.fit(X=df_train, y=y_train)
 
-    # Saving the trained model and encoder on disk
+    # Guardando el modelo entrenado y el codificador en disco
     with open('./model/model.pkl', 'wb') as model_file:
         dill.dump(model, model_file)
 
     with open('./model/encoder.pkl', 'wb') as encoder_file:
         dill.dump(le, encoder_file)
 
-    return 'Model trained successfully'
+    return 'Modelo entrenado exitosamente'
 
 if __name__ == '__main__':
     app.run(debug=True)
-
